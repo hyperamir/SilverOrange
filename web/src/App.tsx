0748a5bf08a55ae4c7c3041b901465d6722b7a70
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-05-06 18:19:13 -0400

    initial commit, fetch data from backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 export function App() {
+  async function fetchData() {
+    try {
+      const result = await axios.get('http://localhost:4000/repos');
+      console.log(result.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
